4dd470e70c3ccdf579afa1beae0da9291a3df7be
Lex <74861308+Kekistanstan@users.noreply.github.com>
2025-01-29 22:47:58 +0800

    with DOM
    delete, add, modify

--- 1smallLibraryApp/a.js
+++ 1smallLibraryApp/a.js
@@ -1,17 +1,80 @@
- const myLibrary = [];
+const movie = [];
 
- function book(books){
-  this.books = books;
- }
+function submit(){
+  const title = document.getElementById("textinput").value;
+  const score = document.getElementById("numinput").value;
 
- book.prototype.addbooktoLibrary = function(){
-  myLibrary.push(this.books);
- };
+  if(title === "" || score === "" || isNaN(score) || score === -1 || score > 10){
+    alert("Enter valid input");
+    return;
+  } 
 
- book.prototype.showlibrary = function(){
-  console.log(myLibrary);
- }
+  const mymovies = new mymovie(title, score);
+  mymovies.addtocollection();
+  mymovies.displaycontent();
+}
 
- const libs = new book("OPM");
- libs.addbooktoLibrary();
- libs.showlibrary();
+function mymovie (title, score){
+  this.title = title;
+  this.score = score;
+}
+
+mymovie.prototype.addtocollection = function(){
+  movie.push({title:this.title, score:this.score, status:this.status});
+};
+
+mymovie.prototype.displaycontent = function(){
+  const table = document.getElementById("table");
+  table.innerHTML = "";
+
+  movie.forEach((movies, index) => {
+    const newrow = document.createElement("tr");
+    const newtitle = document.createElement("td");
+    const newscore = document.createElement("td");
+    const newstatus = document.createElement("button");
+    const newbtn = document.createElement("button");
+
+    newtitle.textContent = movies.title;
+    newscore.textContent = movies.score;
+    newstatus.textContent = movies.status;
+    newbtn.textContent = "delete";
+
+    newstatus.onclick = function(){
+      mymovie.prototype.newstatus(index);
+    };
+
+    newbtn.onclick = function(){
+      mymovie.prototype.delete(index);
+    };
+    
+    newrow.appendChild(newtitle);
+    newrow.appendChild(newscore);
+    newrow.appendChild(newstatus);
+    newrow.appendChild(newbtn);
+    table.appendChild(newrow);
+
+    console.log(movie.title + " " + movie.score);
+  });
+};
+
+mymovie.prototype.newstatus = function(index){
+  if(movie[index].status === "read"){
+    movie[index].status = "not read";
+    this.displaycontent();
+    return;
+  } else if(movie[index].status === "not read"){
+    movie[index].status = "read";
+    this.displaycontent();
+    return;
+  }
+  movie[index].score = "read";
+  this.displaycontent();
+  return;
+}
+
+mymovie.prototype.delete = function(index){
+  movie.splice(index, 1);
+  this.displaycontent();
+  return;
+};
+
