da3a0c915ee9d7dba98072977f3298b763b22386
Lex <74861308+Kekistanstan@users.noreply.github.com>
2025-02-07 00:11:14 +0800

    Update a.js

--- 2TicTacToe/a.js
+++ 2TicTacToe/a.js
@@ -2,11 +2,23 @@ const container = document.getElementById('container');
 for (let i = 0; i <= 8; i++) {
   let cell = document.createElement('div');
   cell.classList.add('cell');
-  cell.addEventListener('click', ox);
+  cell.textContent = "X";
   container.appendChild(cell);
+  cell.addEventListener('click', (event)=>{
+    const Ox = ox(event.target);
+    Ox.tiktoks();
+  });
 }
 
-function ox(event) {
-  let cell = event.target;
-  cell.textContent = "o";
+function ox(cell) {
+  return{
+    cell,
+    tiktoks: function(){
+      if (cell.textContent === "X") {
+        cell.textContent = "O";
+      } else {
+        cell.textContent = "X";
+      }
+    },
+  }
 }
