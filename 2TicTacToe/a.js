83935fe5e54de8fe9197cf9cf8d0001c59eaf0c6
Lex <74861308+Kekistanstan@users.noreply.github.com>
2025-02-07 21:23:01 +0800

    Update a.js

--- 2TicTacToe/a.js
+++ 2TicTacToe/a.js
@@ -1,24 +1,75 @@
 const container = document.getElementById('container');
-for (let i = 0; i <= 8; i++) {
+let turn = "X";
+let table = new Array(9).fill(null);
+
+for (let i = 0; i < 9; i++) {
   let cell = document.createElement('div');
   cell.classList.add('cell');
-  cell.textContent = "X";
   container.appendChild(cell);
+  cell.dataset.index = i;
   cell.addEventListener('click', (event)=>{
-    const Ox = ox(event.target);
-    Ox.tiktoks();
+    const Mark = mark(event.target);
+    Mark.tiktoks();
   });
 }
 
-function ox(cell) {
-  return{
+function mark(cell) {
+  let index = cell.dataset.index;
+  if (cell.textContent !== "") return;
+  return {
     cell,
-    tiktoks: function(){
-      if (cell.textContent === "X") {
-        cell.textContent = "O";
+    tiktoks(){
+      if (turn === "X") {
+        cell.textContent = turn;
+        table[index] = turn;
+        if (checkwin()) {
+          alert("X wins!");
+          reset();
+        } else if (checkdraw()) {
+          alert("Draw!");
+          reset();
+        }
+        turn = "O";
       } else {
-        cell.textContent = "X";
+        cell.textContent = turn;
+        table[index] = turn;
+        if (checkwin()) {
+          alert("X wins!");
+          reset();
+        } else if (checkdraw()) {
+          alert("Draw!");
+          reset();
+        }
+        turn = "X";
       }
-    },
+    }
   }
+}
+
+function checkwin (){
+  const win = [
+    [0,1,2], [3,4,5], [6,7,8], //Horizontal
+    [0,3,6], [1,4,7], [2,5,8], //Vertical
+    [0,4,8], [2,4,6]           //Diagonal
+  ];
+
+  return win.some((pattern) => {
+    const [a, b, c] = pattern;
+    if (table[a] !== null && table[a] === table[b] && table[a] === table[c]){
+      return true;
+    }
+    else return false;
+  });
+}
+
+function checkdraw(){
+  return table.every((cell) => cell !== null);
+}
+
+function reset(){
+  let cells = document.querySelectorAll('.cell');
+  return cells.forEach(element => {
+    element.textContent = "";
+    table = new Array(9).fill(null);
+  });
 }
